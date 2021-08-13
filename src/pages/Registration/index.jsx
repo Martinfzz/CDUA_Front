25831c8ea8ce1e876a3f2c878912b800c4af93ec
Martinfzz <forget.l.martin@gmail.com>
2021-08-13 15:34:14 +0200

    Add input and button in registration

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,23 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView, TextInput, Button } from "react-native";
 const Registration = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOnPress = () => {
+    console.log(email);
+    console.log(password);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>La page Registration</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Email"
+
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry={true}
+        placeholder="Mot de passe"
+      />
+      <Button
+        onPress={handleOnPress}
+        title="S'inscrire"
+        color="#841584"
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
 
 
