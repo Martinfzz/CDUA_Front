e443a44952669269663143076a867f49d6775c92
Martinfzz <forget.l.martin@gmail.com>
2021-08-13 15:56:27 +0200

    Add axios fetch

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,5 @@
 import UserManager from "./user";
-import UiManager from "./ui";
-import MealsManager from "./meals";
-import QuantitiesManager from "./quantities";
 
-// eslint-disable-next-line import/prefer-default-export
 export {
-  UserManager, UiManager, MealsManager, QuantitiesManager,
+  UserManager,
 };
