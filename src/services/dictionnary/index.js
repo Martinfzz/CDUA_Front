a2311933a9e087a673240ff71d1037c856594252
Martinfzz <forget.l.martin@gmail.com>
2021-09-29 14:53:31 +0200

    Search in DB if not fetch in Pons API + add to DB

--- src/services/dictionnary/index.js
+++ src/services/dictionnary/index.js
@@ -2,11 +2,21 @@ import API from "../api";
 
 export default class DictionnaryManager {
   static async dictionnarySearch(word) {
+    console.log("dslihjdlfsijfdslj")
     try {
-      const response = await API.get(`https://api.pons.com/v1/dictionary?&q=${word}&l=defr&fm=1&ref=true`);
+      const response = await API.get(`/dictionnary/${word.toLowerCase()}`);
+      console.log("success")
+
       return response.data;
     } catch (error) {
-      return error;
+      try {
+        const response = await API.get(`https://api.pons.com/v1/dictionary?&q=${word}&l=defr&fm=1&ref=true`);
+        API.post("/dictionnaries", { dictionnary: { title: response.data[0].hits[0].roms[0].headword.toLowerCase(), lang: response.data[0].lang, data: [response.data[0]]}})
+        return response.data;
+      } catch (error) {
+        return error;
+      }
     }
+    
   }
 }
